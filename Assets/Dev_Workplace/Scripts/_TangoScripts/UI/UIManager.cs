358a1c101fde0eae66b6f1bf0ece3c0b3b285a1d
Tangoes98 <cyriltan985@gmail.com>
2024-04-28 00:17:25 -0400

    Update UI icon / button sprites

--- Assets/Dev_Workplace/Scripts/_TangoScripts/UI/UIManager.cs
+++ Assets/Dev_Workplace/Scripts/_TangoScripts/UI/UIManager.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    [Header("REFERENCE")]
+    [Header("MouseCursor")]
+    [SerializeField] Texture2D _mouseCursorDefault;
+    [SerializeField] Texture2D _mouseCursorSelected;
+    Vector2 _hotSpot = Vector2.zero;
+    CursorMode _cursorMode = CursorMode.Auto;
+
+    [Space(10f)]
+    [Header("Resource")]
     [SerializeField] TextMeshProUGUI _currentResource;
+
+    [Space(10f)]
+    [Header("Locolization")]
     [SerializeField] List<TextMeshProUGUI> _ENTexts;
     [SerializeField] List<TextMeshProUGUI> _CNTexts;
 
 
+    private void Start()
+    {
+        Cursor.SetCursor(_mouseCursorDefault, _hotSpot, _cursorMode);
 
+    }
     private void Update()
     {
         UpdatePlayerResourcePanel();
+    }
+    void OnMouseEnter()
+    {
     }
 
 
