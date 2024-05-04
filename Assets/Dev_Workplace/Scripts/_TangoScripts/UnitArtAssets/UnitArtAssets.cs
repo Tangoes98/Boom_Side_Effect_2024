59409a71048e568854f7b3178a69fc9619412cce
Tangoes98 <cyriltan985@gmail.com>
2024-05-04 18:46:41 -0400

    Add Unit attack VFX

---
+++ Assets/Dev_Workplace/Scripts/_TangoScripts/UnitArtAssets/UnitArtAssets.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitArtAssets : MonoBehaviour
+{
+    public bool _IsAOE;
+    public GameObject _AttackVFX;
+}
